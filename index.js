cb26b02b3138ca2b2f3de936aecb866e9450c094
imoranmo <imoranmo@gmail.com>
2021-08-09 19:51:37 -0700

    git ignore changes

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of the project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions',
+      },
+      {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select license',
+        choices: ['No License', 'Apache', 'Boost', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'IBM', 'ISC', 'MIT', 'Mozilla']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter github username.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter email address.',
+      },
+  ])
+
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
